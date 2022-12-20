3c9b2cb28fe149b956823cb964fa84e53457934b
loifloro <jlois.floro@gmail.com>
2022-12-20 15:24:32 +0800

    Initial commit

---
+++ dist/includes/reports/deworming.php
@@ -0,0 +1,48 @@
+<!-- Deworming - report -->
+
+
+<div id='deworming-modal' class="modal deworming-report__table">
+            <ul class="deworming-report__table__row deworming-report__header" role="list">
+                <li class="deworming-report__table__item">
+                    Name
+                </li>
+                <li class="deworming-report__table__item">
+                    Sex
+                </li>
+                <li class="deworming-report__table__item">
+                    Age
+                </li>
+                <li class="deworming-report__table__item">
+                    Birthdate
+                </li>
+                <li class="deworming-report__table__item">
+                    Address
+                </li>
+                <li class="deworming-report__table__item">
+                    Date Availed
+                </li>
+            </ul>
+            <ul class="deworming-report__table__row deworming-report__item" role="list">
+                <li class="deworming-report__table__item">
+                    <?= $patient['firstname']." " . $patient['lastname']; ?>
+                </li>
+                <li class="deworming-report__table__item">
+                    <?= $patient['sex']; ?>
+                </li>
+                <li class="deworming-report__table__item">
+                    <?= $patient['age']; ?>
+                </li>
+                <li class="deworming-report__table__item">
+                    <?= $patient['birthdate']; ?>
+                </li>
+                <li class="deworming-report__table__item">
+                    <?= $patient['street_address'] ." ". $patient['barangay'] ." ". $patient['city']; ?>
+                </li>
+                <li class="deworming-report__table__item">
+                    <?= $patient['deworming_date']; ?>
+                </li>
+            </ul>
+</div>
+
+
+        <!-- <p><a href="#ex1" rel="modal:open">Open Modal</a></p> -->
---
+++ dist/includes/reports/search-and-destroy.php
@@ -0,0 +1,55 @@
+<!-- Search Destroy Modal -->
+<div id='search-and-destroy-modal' class="modal search-and-destroy-report__table">
+            <ul class="search-and-destroy-report__table__row search-and-destroy-report__header" role="list">
+                <li class="search-and-destroy-report__table__item">
+                    Name
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    Contact No.
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    Sex
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    Age
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    Birthday
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    Address
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    Date Availed
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    <!-- Option -->
+                </li>
+            </ul>
+            <ul class="search-and-destroy-report__table__row search-and-destroy-report__item" role="list">
+                <li class="search-and-destroy-report__table__item">
+                    Name
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    Contact No.
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    Sex
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    Age
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    Birthday
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    Address
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    Date Availed
+                </li>
+                <li class="search-and-destroy-report__table__item">
+                    <!-- Option -->
+                </li>
+            </ul>
+        </div>
