605bec7d2cccbaace57a79db674f17a99b830b7c
arianne05 <quimpoarianne5@gmail.com>
2022-12-21 13:13:10 +0800

    Added queries to display value into the report

--- dist/includes/reports/search-and-destroy.php
+++ dist/includes/reports/search-and-destroy.php
@@ -43,12 +43,40 @@
         Daily Consolidation for Search and Destroy
     </h4>
 
+    
     <ul class="search-and-destroy__report__summary" role="list">
         <li class="search-and-destroy__report__summary__item">Name of Barangay Visited: </li>
-        <li class="search-and-destroy__report__summary__item">Total No. of Household Visited: </li>
-        <li class="search-and-destroy__report__summary__item">Total No. of Household Positive for Larva: </li>
-        <li class="search-and-destroy__report__summary__item">Purok/Block Coverage: </li>
+        <!-- START QUERY -->
+        <?php
+                $query = "SELECT
+                        count(*) AS total, 
+                        sum(total_positive_larva) AS total_p 
+                        FROM search_destroy";
+                $result = mysqli_query($conn, $query);
+                while($row = mysqli_fetch_array($result)) {  
+        ?>
+        <li class="search-and-destroy__report__summary__item">Total No. of Household Visited: <?php echo $row['total'] ?></li>
+        <li class="search-and-destroy__report__summary__item">Total No. of Household Positive for Larva: <?php echo $row['total_p']; ?></li>
+       
+        <?php
+        }
+        ?>
+        <!-- END QUERY -->
+        <?php 
+                    include 'includes/connection.php';
+                    $query = "SELECT * FROM search_destroy";
+                    $query_run = mysqli_query($conn, $query);
+                    if(mysqli_num_rows($query_run) > 0)
+                    {
+                        foreach($query_run as $patient) // PROBLEM
+                    {
+        ?>
+        <li class="search-and-destroy__report__summary__item">Purok/Block Coverage: <?= $patient['block']; ?></li> <!--not yet fix-->
+        <?php 
+                    }}
+        ?>                
     </ul>
+    
 
     <h4 class="search-and-destroy__report__title">
         DETAILED SUMMARY OF HOUSEHOLD POSITIVE FOR LARVA
@@ -88,9 +116,9 @@
             <tr>
                 <td class="search-and-destroy__report__table--date"><?= $patient['date_visit']; ?></td>
                 <th><?= $patient['owner_name']; ?></th>
-                <td><?= $patient['address']; ?></td>
+                <td><?= $patient['address'].' '. $patient['block']; ?></td>
                 <td><?= $patient['container_name']; ?></td>
-                <td><?= $patient['container_num']; ?></td>
+                <td><?= $patient['total_positive_larva']; ?></td>
             </tr>
         </tbody>
         <?php 
