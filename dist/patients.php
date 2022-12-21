5034638d05feeeb65a96f3ad27de0a448af817ba
arianne05 <quimpoarianne5@gmail.com>
2022-12-21 11:21:22 +0800

    Added queries to display all data from multiple tables

--- dist/patients.php
+++ dist/patients.php
@@ -129,9 +129,28 @@ if (isset($_SESSION['account_id']) && isset($_SESSION['phone_num'])) {
     <!-- Contents -->
     <main class="patient">
         <section class="patient">
+        <?php
+            include "includes/connection.php";
+                $query = "SELECT 
+                (select count(*) FROM deworming) + 
+                (select count(*) FROM consultation) +
+                (select count(*) FROM early_childhood) +
+                (select count(*) FROM postnatal) +
+                (select count(*) FROM prenatal) +
+                (select count(*) FROM search_destroy) +
+                (select count(*) FROM target_childcare_female) +
+                (select count(*) FROM target_childcare_male) +
+                (select count(*) FROM target_maternal)
+                As total";
+                $result = mysqli_query($conn, $query);
+                while($row = mysqli_fetch_array($result)) {  
+        ?>
             <p class="patient__total">
-                Total patients: <span class="patients__total--num h3">71</span>
+                Total records: <span class="patients__total--num h3"><?php echo $row['total']; ?></span>
             </p>
+        <?php
+            }
+        ?>
             <div class="patient__table">
                 <ul class="patient__table__row patient__attributes" role="list">
                     <li class="patient__attributes__item">
@@ -139,7 +158,7 @@ if (isset($_SESSION['account_id']) && isset($_SESSION['phone_num'])) {
                         <!-- Put SVG here -->
                     </li>
                     <li class="patient__attributes__item">
-                        Contact Number
+                        Date Availed
                     </li>
                     <li class="patient__attributes__item">
                         Sex
@@ -155,20 +174,36 @@ if (isset($_SESSION['account_id']) && isset($_SESSION['phone_num'])) {
                 <!-- Start Query -->
                 <?php 
                     include 'includes/connection.php';
-                    $query = "SELECT deworming_id, firstname, lastname, sex, deworming_date FROM deworming 
+
+                    $query = "SELECT deworming_id, firstname, lastname, sex, deworming_date, label FROM deworming
                             UNION ALL 
-                            SELECT consultation_id, firstname, lastname, sex, consultation_date FROM consultation 
+                            SELECT consultation_id, firstname, lastname, sex, consultation_date, label FROM consultation 
+                            UNION ALL 
+                            SELECT prenatal_id, firstname , lastname, sex, prenatal_date, label FROM prenatal
+                            UNION ALL 
+                            SELECT postnatal_id, firstname, lastname, sex, postnatal_date, label FROM postnatal
+                            UNION ALL 
+                            SELECT search_destroy_id, owner_name, block, barangay, date_visit, label FROM search_destroy
+                            UNION ALL 
+                            SELECT early_childhood_id, child_name, mother_name, sex, child_birthdate, label FROM early_childhood
+                            UNION ALL 
+                            SELECT target_childcare_female_id, child_firstname, child_lastname, sex, birthday, label FROM target_childcare_female
+                            UNION ALL 
+                            SELECT target_childcare_male_id, child_firstname, child_lastname, sex, birthday, label FROM target_childcare_male
+                            UNION ALL 
+                            SELECT target_maternal_id, firstname, lastname, middle_initial, date_registered, label FROM target_maternal
                             ORDER BY firstname"; //USING UNION ALL TO DISPLAY TWO TABLES'S DATA   
                     $query_run = mysqli_query($conn, $query);
 
                         if(mysqli_num_rows($query_run) > 0)
                         {
                         foreach($query_run as $patient)
-                        {
+                        {   
                 ?>
                 <ul class="patient__table__row patient__info" role="list">
                     
                     <li class="patient__name p-bold">
+                        
                         <?= $patient['firstname']." " . $patient['lastname']; ?> <!--Concatenated Firstname and Lastname-->
                     </li>
                     <li class="patient__num">
@@ -178,12 +213,16 @@ if (isset($_SESSION['account_id']) && isset($_SESSION['phone_num'])) {
                         <?= $patient['sex']; ?> <!--Patient Sex-->
                     </li>
                     <li class="patient__availed-service">
-                        <span class="patient__availed-service--deworming">Deworming</span>
-                        <span class="patient__availed-service--prenatal">Prenatal</span>
+                        <span class="patient__availed-service--deworming"><?= $patient['label']; ?></span>
+                        <!-- <span class="patient__availed-service--prenatal">Prenatal</span> -->
                     </li>
                     <li class="patient__option">
-                        <svg class='delete-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M3.389 7.113L4.49 18.021c.061.461 2.287 1.977 5.51 1.979 3.225-.002 5.451-1.518 5.511-1.979l1.102-10.908C14.929 8.055 12.412 8.5 10 8.5c-2.41 0-4.928-.445-6.611-1.387zm9.779-5.603l-.859-.951C11.977.086 11.617 0 10.916 0H9.085c-.7 0-1.061.086-1.392.559l-.859.951C4.264 1.959 2.4 3.15 2.4 4.029v.17C2.4 5.746 5.803 7 10 7c4.198 0 7.601-1.254 7.601-2.801v-.17c0-.879-1.863-2.07-4.433-2.519zM12.07 4.34L11 3H9L7.932 4.34h-1.7s1.862-2.221 2.111-2.522c.19-.23.384-.318.636-.318h2.043c.253 0 .447.088.637.318.248.301 2.111 2.522 2.111 2.522h-1.7z"/></svg>
-                        <svg class='edit-icon' xmlns="http://www.w3.org/2000/svg" width="64pt" height="64pt" viewBox="0 0 64 64" style="isolation:isolate"><defs><clipPath id="a"><rect width="64" height="64"/></clipPath></defs><g clip-path="url(#a)"><path d="M43.926 8.803L49.563 3.167C51.118 1.611 53.643 1.611 55.199 3.167L60.835 8.803C62.39 10.358 62.382 12.876 60.817 14.421L55.146 20.022C54.624 20.537 53.78 20.535 53.261 20.016L43.926 10.681C43.408 10.163 43.408 9.321 43.926 8.803zM42.048 12.56L51.441 21.954C51.96 22.472 51.96 23.314 51.441 23.833L15.276 59.998C15.017 60.257 14.511 60.51 14.148 60.562L4.285 61.971C2.834 62.178 1.823 61.168 2.031 59.716L3.44 49.853C3.492 49.49 3.744 48.985 4.003 48.726L40.169 12.56C40.687 12.042 41.529 12.042 42.048 12.56z"/></g></svg>
+                    <svg class='archive-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M18.521 1.478a1 1 0 0 0-1.414 0L1.48 17.107a1 1 0 1 0 1.414 1.414L18.52 2.892a1 1 0 0 0 0-1.414zM3.108 13.498l2.56-2.56A4.18 4.18 0 0 1 5.555 10c0-2.379 1.99-4.309 4.445-4.309.286 0 .564.032.835.082l1.203-1.202A12.645 12.645 0 0 0 10 4.401C3.44 4.4 0 9.231 0 10c0 .423 1.057 2.09 3.108 3.497zm13.787-6.993l-2.562 2.56c.069.302.111.613.111.935 0 2.379-1.989 4.307-4.444 4.307-.284 0-.56-.032-.829-.081l-1.204 1.203c.642.104 1.316.17 2.033.17 6.56 0 10-4.833 10-5.599 0-.424-1.056-2.09-3.105-3.495z"/></svg>
+                        
+                        <a href="patient-profile.php?id=<?= $target ?>"> <!--MAIN PROBLEM-->   
+                            <svg class='edit-icon' xmlns="http://www.w3.org/2000/svg" width="64pt" height="64pt" viewBox="0 0 64 64" style="isolation:isolate"><defs><clipPath id="a"><rect width="64" height="64"/></clipPath></defs><g clip-path="url(#a)"><path d="M43.926 8.803L49.563 3.167C51.118 1.611 53.643 1.611 55.199 3.167L60.835 8.803C62.39 10.358 62.382 12.876 60.817 14.421L55.146 20.022C54.624 20.537 53.78 20.535 53.261 20.016L43.926 10.681C43.408 10.163 43.408 9.321 43.926 8.803zM42.048 12.56L51.441 21.954C51.96 22.472 51.96 23.314 51.441 23.833L15.276 59.998C15.017 60.257 14.511 60.51 14.148 60.562L4.285 61.971C2.834 62.178 1.823 61.168 2.031 59.716L3.44 49.853C3.492 49.49 3.744 48.985 4.003 48.726L40.169 12.56C40.687 12.042 41.529 12.042 42.048 12.56z"/></g></svg>
+                        </a>
+                        
                     </li>
                         
                 </ul>
