f944f03239ae4049035218a7f0f140e63a020c46
loifloro <jlois.floro@gmail.com>
2022-12-23 10:31:47 +0800

    Added .sort-icon for childcare tab

--- dist/services-consultation.php
+++ dist/services-consultation.php
@@ -535,10 +535,12 @@ if (isset($_SESSION['account_id']) && isset($_SESSION['phone_num'])) {
                 </li>
                 <li class="services__attributes__item">
                     Phone Number
+                    <svg class='sort-icon' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M16.29,14.29,12,18.59l-4.29-4.3a1,1,0,0,0-1.42,1.42l5,5a1,1,0,0,0,1.42,0l5-5a1,1,0,0,0-1.42-1.42ZM7.71,9.71,12,5.41l4.29,4.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42l-5-5a1,1,0,0,0-1.42,0l-5,5A1,1,0,0,0,7.71,9.71Z"/></svg>
                     <!-- Put SVG here -->
                 </li>
                 <li class="services__attributes__item">
                     Child Bithdate
+                    <svg class='sort-icon' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M16.29,14.29,12,18.59l-4.29-4.3a1,1,0,0,0-1.42,1.42l5,5a1,1,0,0,0,1.42,0l5-5a1,1,0,0,0-1.42-1.42ZM7.71,9.71,12,5.41l4.29,4.3a1,1,0,0,0,1.42,0,1,1,0,0,0,0-1.42l-5-5a1,1,0,0,0-1.42,0l-5,5A1,1,0,0,0,7.71,9.71Z"/></svg>
                     <!-- Put SVG here -->
                 </li>
                 <li>
