146628606f76fdcb5dfb9ed1050e79bc8aacc291
loifloro <jlois.floro@gmail.com>
2022-12-25 12:29:55 +0800

    Updated $unmae to $uname

--- dist/includes/connection.php
+++ dist/includes/connection.php
@@ -1,10 +1,10 @@
 <?php
 $sname= "localhost";
-$unmae= "root";
+$uname= "root";
 $password= "";
 
 $db_name= "patient_record_system";
-$conn= mysqli_connect($sname, $unmae, $password, $db_name);
+$conn= mysqli_connect($sname, $uname, $password, $db_name);
 
 if (!$conn) {
     echo "Connection Failed!";
