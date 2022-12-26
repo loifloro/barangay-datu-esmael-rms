cb6e4a99667fff942a7c3af4c23da30768034b2c
arianne05 <quimpoarianne5@gmail.com>
2022-12-26 17:49:27 +0800

    Create function for total patients in patients.php

--- dist/includes/functions.php
+++ dist/includes/functions.php
@@ -1,5 +1,5 @@
-<!-- QUERY TO HIDE CONTENT FOR BHW ACCOUNT -->
 <?php
+    // HIDE CONTENT FOR BHW ACCOUNT
     function hide_content(){
         include 'includes/connection.php';
         $query = "SELECT * FROM account_information WHERE account_id = '".$_SESSION['account_id']."'";
@@ -19,8 +19,28 @@
                 display:none;}
             </style>
     <?php
+            }
         }
     }
-}
-    ?>
-<!-- END OF QUERY -->
+
+    // TOTAL PATIENTS IN PATIENT.PHP
+    function total_patient(){
+        include 'includes/connection.php';
+        $query = "SELECT 
+                (select count(*) FROM deworming) + 
+                (select count(*) FROM consultation) +
+                (select count(*) FROM early_childhood) +
+                (select count(*) FROM postnatal) +
+                (select count(*) FROM prenatal) +
+                (select count(*) FROM search_destroy)
+                As total";
+                $result = mysqli_query($conn, $query);
+                while($row = mysqli_fetch_array($result)) {  
+        ?>
+            <p class="patient__total">
+                Total records: <span class="patients__total--num h3"><?php echo $row['total']; ?></span>
+            </p>
+        <?php
+            }
+    }
+?>
