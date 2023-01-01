cd31a4a6b52bc69bf71306e71ff3c3df1aa10a3c
arianne05 <quimpoarianne5@gmail.com>
2023-01-01 20:55:50 +0800

    Update queries for profiles based on user access

--- dist/includes/patient_profile/profile_prenatal.php
+++ dist/includes/patient_profile/profile_prenatal.php
@@ -25,7 +25,7 @@
                 <?= $patient['firstname']." " . $patient['lastname']; ?>
             </li>
             <li class="patient-profile__contact">
-            <?= $patient['phone_num']; ?>
+                <?= $patient['phone_num']; ?>
             </li>
         </ul>
         
@@ -40,7 +40,7 @@
             </li>
             <li class="patient-profile__last-date-added">
                 <span class="patient-profile__category">Date Added</span>
-                09/15/08
+                    <?= $patient['prenatal_date']; ?>
             </li>
         </ul>
         <ul class="patient-profile__item" role="list">
@@ -91,11 +91,11 @@
             <?php
                         // $patient_fname = $patient['firstname'];
                         // $patient_lname = $patient['lastname'];
+                        // $patient_label = $patient['label'];
                         // $query3 = "SELECT * FROM recent_activity WHERE patient_fname ='$patient_fname' AND patient_lname ='$patient_lname' GROUP BY record_name
                         // ORDER BY recent_activity_id";
                         // $query_run3 = mysqli_query($conn, $query3);
                         //     if(mysqli_num_rows($query_run3) > 0){
-                        //         foreach($query_run3 as $recent2){
 
                             $filtervalues = $patient['firstname'&&'lastname'&&'label'];
                             $query3 = "SELECT deworming_id, firstname, lastname, deworming_date, sex, phone_num, label 
@@ -124,7 +124,7 @@
                                       ";
                             $query_run3 = mysqli_query($conn, $query3); 
                             if(mysqli_num_rows($query_run) > 0){
-                                foreach($query_run3 as $recent3){
+                                foreach($query_run3 as $recent3){                                  
                     ?>
             <ul class="medical-history__card__row" role="list">
                 <li class="medical-history__item medical-history__service p-bold">
@@ -133,12 +133,13 @@
                 <li class="medical-history__item medical-history__service__date-availed">
                     <?= $patient['prenatal_date']; ?>
                 </li>
-                <!-- START QUERY FOR EDIT SERVICES-->
-                <?php
+                <li class="medical-history__item medical-history__btn">
+                    <!-- START QUERY FOR EDIT SERVICES-->
+                    <?php
                         if(isset($_GET['label'])){
                             $patient_label = mysqli_real_escape_string($conn, $_GET['label']);
                             $changes_label=$recent3['label'];
-                            
+
                                 //DEWORMING
                                 if(($changes_label == "Deworming") AND (isset($_GET['id']))){
                                     $patient_id = mysqli_real_escape_string($conn, $_GET['id']);
@@ -225,12 +226,16 @@
                            
                     ?>
                     <a href="<?= $link; ?>">
-                        <svg class='edit-icon' xmlns="http://www.w3.org/2000/svg" width="64pt" height="64pt" viewBox="0 0 64 64" style="isolation:isolate"><defs><clipPath id="a"><rect width="64" height="64"/></clipPath></defs><g clip-path="url(#a)"><path d="M43.926 8.803L49.563 3.167C51.118 1.611 53.643 1.611 55.199 3.167L60.835 8.803C62.39 10.358 62.382 12.876 60.817 14.421L55.146 20.022C54.624 20.537 53.78 20.535 53.261 20.016L43.926 10.681C43.408 10.163 43.408 9.321 43.926 8.803zM42.048 12.56L51.441 21.954C51.96 22.472 51.96 23.314 51.441 23.833L15.276 59.998C15.017 60.257 14.511 60.51 14.148 60.562L4.285 61.971C2.834 62.178 1.823 61.168 2.031 59.716L3.44 49.853C3.492 49.49 3.744 48.985 4.003 48.726L40.169 12.56C40.687 12.042 41.529 12.042 42.048 12.56z"/></g></svg>
+                        <svg id="edit-profile" class='edit-icon' xmlns="http://www.w3.org/2000/svg" width="64pt" height="64pt" viewBox="0 0 64 64" style="isolation:isolate"><defs><clipPath id="a"><rect width="64" height="64"/></clipPath></defs><g clip-path="url(#a)"><path d="M43.926 8.803L49.563 3.167C51.118 1.611 53.643 1.611 55.199 3.167L60.835 8.803C62.39 10.358 62.382 12.876 60.817 14.421L55.146 20.022C54.624 20.537 53.78 20.535 53.261 20.016L43.926 10.681C43.408 10.163 43.408 9.321 43.926 8.803zM42.048 12.56L51.441 21.954C51.96 22.472 51.96 23.314 51.441 23.833L15.276 59.998C15.017 60.257 14.511 60.51 14.148 60.562L4.285 61.971C2.834 62.178 1.823 61.168 2.031 59.716L3.44 49.853C3.492 49.49 3.744 48.985 4.003 48.726L40.169 12.56C40.687 12.042 41.529 12.042 42.048 12.56z"/></g></svg>
+                    </a>
+                    <a href="#">
+                    <svg id="archive-profile" class='archive-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M18.521 1.478a1 1 0 0 0-1.414 0L1.48 17.107a1 1 0 1 0 1.414 1.414L18.52 2.892a1 1 0 0 0 0-1.414zM3.108 13.498l2.56-2.56A4.18 4.18 0 0 1 5.555 10c0-2.379 1.99-4.309 4.445-4.309.286 0 .564.032.835.082l1.203-1.202A12.645 12.645 0 0 0 10 4.401C3.44 4.4 0 9.231 0 10c0 .423 1.057 2.09 3.108 3.497zm13.787-6.993l-2.562 2.56c.069.302.111.613.111.935 0 2.379-1.989 4.307-4.444 4.307-.284 0-.56-.032-.829-.081l-1.204 1.203c.642.104 1.316.17 2.033.17 6.56 0 10-4.833 10-5.599 0-.424-1.056-2.09-3.105-3.495z"/></svg>
                     </a>
                     <?php
                          }
                     ?>
                     <!-- END QUERY FOR EDIT SERVICES -->
+                </li>
             </ul>
             <?php
                                 }
--- dist/includes/patient_profile/profile_search_destroy.php
+++ dist/includes/patient_profile/profile_search_destroy.php
@@ -9,7 +9,7 @@
                 $query_run = mysqli_query($conn, $query);
 
                 if(mysqli_num_rows($query_run) > 0){
-                    
+
                     $patient = mysqli_fetch_array($query_run);
         ?>
     <ul class="patient-profile__list" role="list">
@@ -18,7 +18,7 @@
             <li class="patient-profile__img">
                 <img class=""
                         src="./assets/img/patient-profile.svg"
-                        alt=""> 
+                        alt="">
             </li>
             <li class="patient-profile__id patient-profile__category">
                 #<?= $patient['search_destroy_id']; ?>
@@ -90,41 +90,41 @@
             </ul>
             <!-- Query for Medical History -->
             <?php
-                        // $patient_fname = $patient['owner_fname'];
-                        // $patient_lname = $patient['owner_lname'];
+                        // $patient_fname = $patient['firstname'];
+                        // $patient_lname = $patient['lastname'];
+                        // $patient_label = $patient['label'];
                         // $query3 = "SELECT * FROM recent_activity WHERE patient_fname ='$patient_fname' AND patient_lname ='$patient_lname' GROUP BY record_name
                         // ORDER BY recent_activity_id";
                         // $query_run3 = mysqli_query($conn, $query3);
                         //     if(mysqli_num_rows($query_run3) > 0){
-                        //         foreach($query_run3 as $recent2){
 
                             $filtervalues = $patient['firstname'&&'lastname'&&'label'];
                             $query3 = "SELECT deworming_id, firstname, lastname, deworming_date, sex, phone_num, label 
                                       FROM deworming WHERE CONCAT(firstname,lastname,deworming_date,sex,phone_num, label) 
-                                      LIKE '%$filtervalues%' 
+                                      LIKE '%$filtervalues%' GROUP BY label
                                       UNION ALL
                                       SELECT consultation_id, firstname, lastname, consultation_date, sex, phone_number, label 
                                       FROM consultation WHERE CONCAT(firstname,lastname,consultation_date,sex,phone_number, label) 
-                                      LIKE '%$filtervalues%'
+                                      LIKE '%$filtervalues%' GROUP BY label
                                       UNION ALL
                                       SELECT prenatal_id, firstname, lastname, prenatal_date, sex, phone_num, label 
                                       FROM prenatal WHERE CONCAT(firstname,lastname,prenatal_date,sex,phone_num, label) 
-                                      LIKE '%$filtervalues%'
+                                      LIKE '%$filtervalues%' GROUP BY label
                                       UNION ALL
                                       SELECT postnatal_id, firstname, lastname, postnatal_date, sex, phone_num, label 
                                       FROM postnatal WHERE CONCAT(firstname,lastname,postnatal_date,sex,phone_num, label) 
-                                      LIKE '%$filtervalues%'
+                                      LIKE '%$filtervalues%' GROUP BY label
                                       UNION ALL
                                       SELECT search_destroy_id, owner_fname, owner_lname, search_destroy_date, sex, phone_num, label 
                                       FROM search_destroy WHERE CONCAT(owner_fname,owner_lname,search_destroy_date,sex,phone_num, label) 
-                                      LIKE '%$filtervalues%'
+                                      LIKE '%$filtervalues%' GROUP BY label
                                       UNION ALL
                                       SELECT early_childhood_id, child_fname, child_lname, early_childhood_date, sex, phone_num, label 
                                       FROM early_childhood WHERE CONCAT(child_fname,child_lname,early_childhood_date,sex,phone_num, label) 
-                                      LIKE '%$filtervalues%'
+                                      LIKE '%$filtervalues%' GROUP BY label
                                       ";
                             $query_run3 = mysqli_query($conn, $query3); 
-                            if(mysqli_num_rows($query_run) > 0){
+                            if(mysqli_num_rows($query_run3) > 0){
                                 foreach($query_run3 as $recent3){
                     ?>
             <ul class="medical-history__card__row" role="list">
@@ -132,14 +132,15 @@
                     <?= $recent3['label']; ?>
                 </li>
                 <li class="medical-history__item medical-history__service__date-availed">
-                    <?= $patient['date_visit']; ?>
+                    <?= $patient['search_destroy_date']; ?>
                 </li>
-                <!-- START QUERY FOR EDIT SERVICES-->
-                <?php
+                <li class="medical-history__item medical-history__btn">
+                    <!-- START QUERY FOR EDIT SERVICES-->
+                    <?php
                         if(isset($_GET['label'])){
                             $patient_label = mysqli_real_escape_string($conn, $_GET['label']);
                             $changes_label=$recent3['label'];
-                            
+
                                 //DEWORMING
                                 if(($changes_label == "Deworming") AND (isset($_GET['id']))){
                                     $patient_id = mysqli_real_escape_string($conn, $_GET['id']);
@@ -226,12 +227,16 @@
                            
                     ?>
                     <a href="<?= $link; ?>">
-                        <svg class='edit-icon' xmlns="http://www.w3.org/2000/svg" width="64pt" height="64pt" viewBox="0 0 64 64" style="isolation:isolate"><defs><clipPath id="a"><rect width="64" height="64"/></clipPath></defs><g clip-path="url(#a)"><path d="M43.926 8.803L49.563 3.167C51.118 1.611 53.643 1.611 55.199 3.167L60.835 8.803C62.39 10.358 62.382 12.876 60.817 14.421L55.146 20.022C54.624 20.537 53.78 20.535 53.261 20.016L43.926 10.681C43.408 10.163 43.408 9.321 43.926 8.803zM42.048 12.56L51.441 21.954C51.96 22.472 51.96 23.314 51.441 23.833L15.276 59.998C15.017 60.257 14.511 60.51 14.148 60.562L4.285 61.971C2.834 62.178 1.823 61.168 2.031 59.716L3.44 49.853C3.492 49.49 3.744 48.985 4.003 48.726L40.169 12.56C40.687 12.042 41.529 12.042 42.048 12.56z"/></g></svg>
+                        <svg id= "edit-profile" class='edit-icon' xmlns="http://www.w3.org/2000/svg" width="64pt" height="64pt" viewBox="0 0 64 64" style="isolation:isolate"><defs><clipPath id="a"><rect width="64" height="64"/></clipPath></defs><g clip-path="url(#a)"><path d="M43.926 8.803L49.563 3.167C51.118 1.611 53.643 1.611 55.199 3.167L60.835 8.803C62.39 10.358 62.382 12.876 60.817 14.421L55.146 20.022C54.624 20.537 53.78 20.535 53.261 20.016L43.926 10.681C43.408 10.163 43.408 9.321 43.926 8.803zM42.048 12.56L51.441 21.954C51.96 22.472 51.96 23.314 51.441 23.833L15.276 59.998C15.017 60.257 14.511 60.51 14.148 60.562L4.285 61.971C2.834 62.178 1.823 61.168 2.031 59.716L3.44 49.853C3.492 49.49 3.744 48.985 4.003 48.726L40.169 12.56C40.687 12.042 41.529 12.042 42.048 12.56z"/></g></svg>
+                    </a>
+                    <a href="#">
+                        <svg id="archive-profile" class='archive-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M18.521 1.478a1 1 0 0 0-1.414 0L1.48 17.107a1 1 0 1 0 1.414 1.414L18.52 2.892a1 1 0 0 0 0-1.414zM3.108 13.498l2.56-2.56A4.18 4.18 0 0 1 5.555 10c0-2.379 1.99-4.309 4.445-4.309.286 0 .564.032.835.082l1.203-1.202A12.645 12.645 0 0 0 10 4.401C3.44 4.4 0 9.231 0 10c0 .423 1.057 2.09 3.108 3.497zm13.787-6.993l-2.562 2.56c.069.302.111.613.111.935 0 2.379-1.989 4.307-4.444 4.307-.284 0-.56-.032-.829-.081l-1.204 1.203c.642.104 1.316.17 2.033.17 6.56 0 10-4.833 10-5.599 0-.424-1.056-2.09-3.105-3.495z"/></svg>
                     </a>
                     <?php
                          }
                     ?>
                     <!-- END QUERY FOR EDIT SERVICES -->
+                </li>
             </ul>
             <?php
                                 }
@@ -265,7 +270,7 @@
                     <span class="edit-history__editor p-bold">
                         <?= $recent['user_fname'].' '.$recent['user_lname'].' '.$recent['changes_label']; ?>
                     </span>
-                    <span class="edit-history edit-history__action p-bold"><?= $recent['patient_fname'];?> </span>
+                    <span class="edit-history edit-history__action p-bold"><?= $recent['patient_fname'].' '.$recent['patient_lname']; ?> </span>
                     <span class="edit-history__subject"><?= $recent['record_name']; ?> record</span> on
                     <span class="edit-history__date"><?= $recent['date_edit']; ?></span>
                         <hr>
