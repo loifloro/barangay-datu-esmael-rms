e542bd91e2836d0adff0312ecbd775e5009556f4
loifloro <jlois.floro@gmail.com>
2023-01-03 12:52:55 +0800

    Updated $_SESSION to avoid patient access

--- dist/services-consultation.php
+++ dist/services-consultation.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 include 'includes/connection.php';
-if (!isset($_SESSION['account_id']) && !isset($_SESSION['phone_num'])) {
+if ((!isset($_SESSION['account_id']) || !isset($_SESSION['phone_num'])) || !isset($_SESSION['position'])) {
     header("Location: index.php?error=You are not logged in"); /*Redirect to this page if successful*/
     exit();
 }
