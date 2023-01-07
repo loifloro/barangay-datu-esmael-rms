3c35bb1baa2729b329cb517490b69f78b1931c49
loifloro <jlois.floro@gmail.com>
2023-01-07 12:24:04 +0800

    Added modal for daily report

--- dist/includes/reports/search-and-destroy.php
+++ dist/includes/reports/search-and-destroy.php
@@ -19,7 +19,7 @@
     </ul>
     <ul class="search-and-destroy__record__table__row search-and-destroy__record__item" role="list">
         <li class="search-and-destroy__record__table__item">
-            <?= $patient['owner_fname'].' '.$patient['owner_lname']; ?>    
+            <?= $patient['owner_fname'] . ' ' . $patient['owner_lname']; ?>
         </li>
         <li class="search-and-destroy__record__table__item">
             <?= $patient['container_name']; ?>
@@ -31,62 +31,60 @@
             <?= $patient['address']; ?>
         </li>
         <li class="search-and-destroy__record__table__item">
-            <?= $patient['date_visit']; ?>    
+            <?= $patient['date_visit']; ?>
         </li>
     </ul>
 </div>
-
-
 
 <div id="search-and-destroy__report" class="modal search-and-destroy__report">
     <h4 class="search-and-destroy__report__title">
         Daily Consolidation for Search and Destroy
     </h4>
 
-    
+
     <ul class="search-and-destroy__report__summary" role="list">
-        <li class="search-and-destroy__report__summary__item">Name of Barangay Visited: 
+        <li class="search-and-destroy__report__summary__item">Name of Barangay Visited:
             <span class="search-and-destroy__report__summary--value">
                 Datu Esmael
-            </span> 
+            </span>
         </li>
         <!-- START QUERY -->
         <?php
-                $query = "SELECT count(*) AS total, sum(container_num) AS total_p FROM search_destroy WHERE archive_label=''";
-                $result = mysqli_query($conn, $query);
-                while($row = mysqli_fetch_array($result)) {  
+        $query = "SELECT count(*) AS total, sum(container_num) AS total_p FROM search_destroy WHERE archive_label=''";
+        $result = mysqli_query($conn, $query);
+        while ($row = mysqli_fetch_array($result)) {
         ?>
-        <li class="search-and-destroy__report__summary__item">Total No. of Household Visited: <span class="search-and-destroy__report__summary--value"><?php echo $row['total'] ?></span></li>
-        <li class="search-and-destroy__report__summary__item">Total No. of Household Positive for Larva: <span class="search-and-destroy__report__summary--value"><?php echo $row['total_p']; ?></span></li>
-       
+            <li class="search-and-destroy__report__summary__item">Total No. of Household Visited: <span class="search-and-destroy__report__summary--value"><?php echo $row['total'] ?></span></li>
+            <li class="search-and-destroy__report__summary__item">Total No. of Household Positive for Larva: <span class="search-and-destroy__report__summary--value"><?php echo $row['total_p']; ?></span></li>
+
         <?php
         }
         ?>
         <!-- END QUERY -->
         <li class="search-and-destroy__report__summary__item">
-            Purok/Block Coverage: 
+            Purok/Block Coverage:
 
             <span class="search-and-destroy__report__summary--value">
 
-                <?php 
-                    include 'includes/connection.php';
-                    $query = "SELECT * FROM search_destroy WHERE archive_label=''";
-                    $query_run = mysqli_query($conn, $query);
-                    if(mysqli_num_rows($query_run) > 0)
+                <?php
+                include 'includes/connection.php';
+                $query = "SELECT * FROM search_destroy WHERE archive_label=''";
+                $query_run = mysqli_query($conn, $query);
+                if (mysqli_num_rows($query_run) > 0) {
+                    foreach ($query_run as $patient) // PROBLEM
                     {
-                        foreach($query_run as $patient) // PROBLEM
-                        {
-                            ?>
-            
-            <?= $patient['block'] . '. '; ?>
-            
-            <?php 
-                    }}
-                    ?>                
+                ?>
+
+                        <?= $patient['block'] . '. '; ?>
+
+                <?php
+                    }
+                }
+                ?>
             </span>
         </li> <!--not yet fix-->
     </ul>
-    
+
 
     <h4 class="search-and-destroy__report__title">
         DETAILED SUMMARY OF HOUSEHOLD POSITIVE FOR LARVA
@@ -102,7 +100,7 @@
                     Owner of the House
                 </th>
                 <th>
-                    Complete Address 
+                    Complete Address
                 </th>
                 <th>
                     Name of Container Positive for Larva
@@ -113,26 +111,135 @@
             </tr>
         </thead>
         <!-- To be put in the loop -->
-        <?php 
-                    include 'includes/connection.php';
-                    $query = "SELECT * FROM search_destroy WHERE archive_label='' ORDER BY date_visit";
-                    $query_run = mysqli_query($conn, $query);
-                    if(mysqli_num_rows($query_run) > 0)
-                    {
-                        foreach($query_run as $patient)
-                    {
+        <?php
+        include 'includes/connection.php';
+        $query = "SELECT * FROM search_destroy WHERE archive_label='' ORDER BY date_visit";
+        $query_run = mysqli_query($conn, $query);
+        if (mysqli_num_rows($query_run) > 0) {
+            foreach ($query_run as $patient) {
         ?>
-        <tbody class="search-and-destroy__report__table__item">
+                <tbody class="search-and-destroy__report__table__item">
+                    <tr>
+                        <td class="search-and-destroy__report__table--date"><?= $patient['date_visit']; ?></td>
+                        <th><?= $patient['owner_fname'] . ' ' . $patient['owner_lname']; ?></th>
+                        <td><?= $patient['address'] . ' ' . $patient['block']; ?></td>
+                        <td><?= $patient['container_name']; ?></td>
+                        <td><?= $patient['container_num']; ?></td>
+                    </tr>
+                </tbody>
+        <?php
+            }
+        }
+        ?>
+    </table>
+    <button type="submit" class="btn-green btn-add services__btn btn-print" onclick="window.print();">
+        Print
+    </button>
+</div>
+
+
+<!-- Search and Destroy Daily report -->
+<div id="search-and-destroy__daily-report" class="modal search-and-destroy__report">
+    <h4 class="consultation__report__title">
+        City Government of Dasmariñas <br> City Health Office II
+    </h4>
+    <p class="consultation__report__city">
+        City of Dasmariñas, Cavite
+    </p>
+
+    <h4 class="search-and-destroy__report__title">
+        Daily Consolidation for Search and Destroy
+    </h4>
+
+
+    <ul class="search-and-destroy__report__summary" role="list">
+        <li class="search-and-destroy__report__summary__item">Name of Barangay Visited:
+            <span class="search-and-destroy__report__summary--value">
+                Datu Esmael
+            </span>
+        </li>
+        <!-- START QUERY -->
+        <?php
+        $query = "SELECT count(*) AS total, sum(container_num) AS total_p FROM search_destroy WHERE archive_label=''";
+        $result = mysqli_query($conn, $query);
+        while ($row = mysqli_fetch_array($result)) {
+        ?>
+            <li class="search-and-destroy__report__summary__item">Total No. of Household Visited: <span class="search-and-destroy__report__summary--value"><?php echo $row['total'] ?></span></li>
+            <li class="search-and-destroy__report__summary__item">Total No. of Household Positive for Larva: <span class="search-and-destroy__report__summary--value"><?php echo $row['total_p']; ?></span></li>
+
+        <?php
+        }
+        ?>
+        <!-- END QUERY -->
+        <li class="search-and-destroy__report__summary__item">
+            Purok/Block Coverage:
+
+            <span class="search-and-destroy__report__summary--value">
+
+                <?php
+                include 'includes/connection.php';
+                $query = "SELECT * FROM search_destroy WHERE archive_label=''";
+                $query_run = mysqli_query($conn, $query);
+                if (mysqli_num_rows($query_run) > 0) {
+                    foreach ($query_run as $patient) // PROBLEM
+                    {
+                ?>
+
+                        <?= $patient['block'] . '. '; ?>
+
+                <?php
+                    }
+                }
+                ?>
+            </span>
+        </li> <!--not yet fix-->
+    </ul>
+
+
+    <h4 class="search-and-destroy__report__title">
+        DETAILED SUMMARY OF HOUSEHOLD POSITIVE FOR LARVA
+    </h4>
+
+    <table class="search-and-destroy__report__table">
+        <thead class="search-and-destroy__report__table__header">
             <tr>
-                <td class="search-and-destroy__report__table--date"><?= $patient['date_visit']; ?></td>
-                <th><?= $patient['owner_fname'].' '.$patient['owner_lname']; ?></th>
-                <td><?= $patient['address'].' '. $patient['block']; ?></td>
-                <td><?= $patient['container_name']; ?></td>
-                <td><?= $patient['container_num']; ?></td>
+                <th class="search-and-destroy__report__table--date">
+                    Date of Visit
+                </th>
+                <th>
+                    Owner of the House
+                </th>
+                <th>
+                    Complete Address
+                </th>
+                <th>
+                    Name of Container Positive for Larva
+                </th>
+                <th>
+                    No. of Container Positive for Larva
+                </th>
             </tr>
-        </tbody>
-        <?php 
-                    }}
+        </thead>
+        <!-- To be put in the loop -->
+        <?php
+        include 'includes/connection.php';
+        $query = "SELECT * FROM search_destroy WHERE archive_label='' ORDER BY date_visit";
+        $query_run = mysqli_query($conn, $query);
+        if (mysqli_num_rows($query_run) > 0) {
+            foreach ($query_run as $patient) {
+        ?>
+                <tbody class="search-and-destroy__report__table__item">
+                    <tr>
+                        <td class="search-and-destroy__report__table--date"><?= $patient['date_visit']; ?></td>
+                        <th><?= $patient['owner_fname'] . ' ' . $patient['owner_lname']; ?></th>
+                        <td><?= $patient['address'] . ' ' . $patient['block']; ?></td>
+                        <td><?= $patient['container_name']; ?></td>
+                        <td><?= $patient['container_num']; ?></td>
+                    </tr>
+                </tbody>
+        <?php
+            }
+        }
         ?>
     </table>
     <button type="submit" class="btn-green btn-add services__btn btn-print" onclick="window.print();">
