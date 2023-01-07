1918450d04a6fde9ee5b8d2c2a32fb23efaecab6
arianne05 <quimpoarianne5@gmail.com>
2023-01-07 21:41:38 +0800

    Changed range in age

--- dist/includes/daily-reports/prenatal.php
+++ dist/includes/daily-reports/prenatal.php
@@ -28,19 +28,19 @@
         <!-- Query Start -->
         <?php
         //DEFAULT DISPLAY
-        $query = "SELECT count(*) FROM prenatal WHERE archive_label='' AND age>=17 AND age<=21";
+        $query = "SELECT count(*) FROM prenatal WHERE archive_label='' AND age<=17";
         $result = mysqli_query($conn, $query);
 
         //CONDITION IF SORT BUTTON IS CLICKED
         if (isset($_GET['sort__date'])) {
             $date = mysqli_real_escape_string($conn, $_GET['report__date']);
-            $query = "SELECT count(*) FROM prenatal WHERE archive_label='' AND age>=17 AND age<=21 AND prenatal_date='$date'";
+            $query = "SELECT count(*) FROM prenatal WHERE archive_label='' AND age<=17 AND prenatal_date='$date'";
             $result = mysqli_query($conn, $query);
         }
 
         while ($row = mysqli_fetch_array($result)) {
         ?>
-            <p class="reports__card__title">Total No. of Patients age 17-21</p>
+            <p class="reports__card__title">Total No. of Patients age less/equal 17</p>
             <input type="range" name="" id="" max="20" value='<?php echo $row['count(*)']; ?>'>
             <p class="reports__card__total"> <?php echo $row['count(*)']; ?> </p>
         <?php
@@ -52,19 +52,19 @@
         <!-- Query Start -->
         <?php
         //DEFAULT DISPLAY
-        $query = "SELECT count(*) FROM prenatal WHERE archive_label='' AND age>=22 AND age<=29";
+        $query = "SELECT count(*) FROM prenatal WHERE archive_label='' AND age>=18 AND age<=29";
         $result = mysqli_query($conn, $query);
 
         //CONDITION IF SORT BUTTON IS CLICKED
         if (isset($_GET['sort__date'])) {
             $date = mysqli_real_escape_string($conn, $_GET['report__date']);
-            $query = "SELECT count(*) FROM prenatal WHERE archive_label='' AND age>=22 AND age<=29 AND prenatal_date='$date'";
+            $query = "SELECT count(*) FROM prenatal WHERE archive_label='' AND age>=18 AND age<=29 AND prenatal_date='$date'";
             $result = mysqli_query($conn, $query);
         }
 
         while ($row = mysqli_fetch_array($result)) {
         ?>
-            <p class="reports__card__title">Total No. of Patients age 22-29</p>
+            <p class="reports__card__title">Total No. of Patients age 18-29</p>
             <input type="range" name="" id="" max="20" value='<?php echo $row['count(*)']; ?>'>
             <p class="reports__card__total"> <?php echo $row['count(*)']; ?> </p>
         <?php
