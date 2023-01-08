9689d882decbde7a726dd0a82a6b25ae70d73943
arianne05 <quimpoarianne5@gmail.com>
2023-01-08 16:18:11 +0800

    Changed password from lastname to deworming_password

--- dist/login.php
+++ dist/login.php
@@ -42,38 +42,42 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
                 exit();
             } 
         } else {
-            $sql= "SELECT deworming_email, lastname, phone_num, deworming_id, label FROM deworming 
-            WHERE deworming_email='$username' AND lastname='$password'
-            UNION
-            SELECT consultation_email, lastname, phone_number, consultation_id, label FROM consultation 
-            WHERE consultation_email='$username' AND lastname='$password'
-            UNION
-            SELECT prenatal_email, lastname, phone_num, prenatal_id, label FROM prenatal 
-            WHERE prenatal_email='$username' AND lastname='$password'
-            UNION
-            SELECT search_destroy_email, owner_lname, phone_num, search_destroy_id, label FROM search_destroy 
-            WHERE search_destroy_email='$username' AND owner_lname='$password'
-            UNION
-            SELECT early_childhood_email, child_lname, phone_num, early_childhood_id, label FROM early_childhood 
-            WHERE early_childhood_email='$username' AND child_lname='$password'
+            $sql= "SELECT deworming_email, deworming_password, phone_num, deworming_id, label FROM deworming 
+            WHERE deworming_email='$username' AND deworming_password='$password'
+            UNION ALL
+            SELECT consultation_email, consultation_password, phone_number, consultation_id, label FROM consultation 
+            WHERE consultation_email='$username' AND consultation_password='$password'
+            UNION ALL
+            SELECT prenatal_email, prenatal_password, phone_num, prenatal_id, label FROM prenatal 
+            WHERE prenatal_email='$username' AND prenatal_password='$password'
+            UNION ALL
+            SELECT postnatal_email, postnatal_password, phone_num, postnatal_id, label FROM postnatal 
+            WHERE postnatal_email='$username' AND postnatal_password='$password'
+            UNION ALL
+            SELECT search_destroy_email, search_destroy_password, phone_num, search_destroy_id, label FROM search_destroy 
+            WHERE search_destroy_email='$username' AND search_destroy_password='$password'
+            UNION ALL
+            SELECT early_childhood_email, early_childhood_password, phone_num, early_childhood_id, label FROM early_childhood 
+            WHERE early_childhood_email='$username' AND early_childhood_password='$password'
             ";
 
             $result= mysqli_query($conn, $sql);
             if (mysqli_num_rows($result) == 1) {
                 $row = mysqli_fetch_assoc($result);
 
-                if ($row['lastname'] == $password && $row['deworming_email'] == $username){
-                    $_SESSION['phone_num'] = $row['phone_num'];
-                    $_SESSION['firstname'] = $row['firstname'];
-                    $_SESSION['deworming_id'] = $row['deworming_id'];
-                    $_SESSION['label'] = $row['label'];
-                    header("Location: search-result.php?success"); /*Redirect to this page if successful*/
-                    exit();
-                } 
-                else {
-                    header("Location: index.php?error=No patient"); /*Error Alert*/
-                    exit();
-                }
+                    if ($row['deworming_password'] == $password && $row['deworming_email'] == $username){
+                        $_SESSION['phone_num'] = $row['phone_num'];
+                        $_SESSION['firstname'] = $row['firstname'];
+                        $_SESSION['deworming_id'] = $row['deworming_id'];
+                        $_SESSION['label'] = $row['label'];
+                        
+                        header("Location: search-result.php?success"); /*Redirect to this page if successful*/
+                        exit();
+                    } 
+                    else {
+                        header("Location: index.php?error=No patient"); /*Error Alert*/
+                        exit();
+                    }
             }
         }
         }
