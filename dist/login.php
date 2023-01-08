324522d99ead8ec501f7a4e6dcdf0b6f0f00b072
loifloro <jlois.floro@gmail.com>
2023-01-08 18:57:40 +0800

    Added UNION ALL to variable $sql

--- dist/login.php
+++ dist/login.php
@@ -3,7 +3,8 @@ session_start();
 include "includes/connection.php";
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
-    function validate($data){
+    function validate($data)
+    {
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
@@ -16,15 +17,12 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     if (empty($username)) { /*if else statement for validation*/
         header("Location: index.php?error=Contact number is required"); /*Error Alert*/
         exit();
-    } 
-
-    else if (empty($password)){
+    } else if (empty($password)) {
         header("Location: index.php?error=Password is required"); /*Error Alert*/
         exit();
-    } 
-    else {
-        $sql= "SELECT * FROM account_information WHERE user_email='$username' AND password='$password'";
-        $result= mysqli_query($conn, $sql);
+    } else {
+        $sql = "SELECT * FROM account_information WHERE user_email='$username' AND password='$password'";
+        $result = mysqli_query($conn, $sql);
 
         if (mysqli_num_rows($result) == 0) {
             header("Location: index.php?error=Incorrect username or password!");
@@ -32,7 +30,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 
         if (mysqli_num_rows($result) == 1) {
             $row = mysqli_fetch_assoc($result);
-            if ($row['password'] == $password && $row['user_email'] == $username){
+            if ($row['password'] == $password && $row['user_email'] == $username) {
                 $_SESSION['user_email'] = $row['user_email'];
                 $_SESSION['phone_num'] = $row['phone_num'];
                 $_SESSION['firstname'] = $row['firstname'];
@@ -40,47 +38,43 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
                 $_SESSION['position'] = $row['position'];
                 header("Location: dashboard.php?success"); /*Redirect to this page if successful*/
                 exit();
-            } 
+            }
         } else {
-            $sql= "SELECT deworming_email, lastname, phone_num, deworming_id, label FROM deworming 
+            $sql = "SELECT deworming_email, lastname, phone_num, deworming_id, label FROM deworming 
             WHERE deworming_email='$username' AND lastname='$password'
-            UNION
+            UNION ALL
             SELECT consultation_email, lastname, phone_number, consultation_id, label FROM consultation 
             WHERE consultation_email='$username' AND lastname='$password'
-            UNION
+            UNION ALL
             SELECT prenatal_email, lastname, phone_num, prenatal_id, label FROM prenatal 
             WHERE prenatal_email='$username' AND lastname='$password'
-            UNION
+            UNION ALL
             SELECT search_destroy_email, owner_lname, phone_num, search_destroy_id, label FROM search_destroy 
             WHERE search_destroy_email='$username' AND owner_lname='$password'
-            UNION
+            UNION ALL
             SELECT early_childhood_email, child_lname, phone_num, early_childhood_id, label FROM early_childhood 
             WHERE early_childhood_email='$username' AND child_lname='$password'
             ";
 
-            $result= mysqli_query($conn, $sql);
+            $result = mysqli_query($conn, $sql);
             if (mysqli_num_rows($result) == 1) {
                 $row = mysqli_fetch_assoc($result);
 
-                if ($row['lastname'] == $password && $row['deworming_email'] == $username){
+                if ($row['lastname'] == $password && $row['deworming_email'] == $username) {
                     $_SESSION['phone_num'] = $row['phone_num'];
                     $_SESSION['firstname'] = $row['firstname'];
                     $_SESSION['deworming_id'] = $row['deworming_id'];
                     $_SESSION['label'] = $row['label'];
                     header("Location: search-result.php?success"); /*Redirect to this page if successful*/
                     exit();
-                } 
-                else {
+                } else {
                     header("Location: index.php?error=No patient"); /*Error Alert*/
                     exit();
                 }
             }
         }
-        }
-
-}
-
-else{
+    }
+} else {
     header("Location: index.php");
     exit();
 }
