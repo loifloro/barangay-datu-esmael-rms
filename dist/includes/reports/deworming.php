568f54e3381be456fe0c6523d6c4c4f1d131cf4c
arianne05 <quimpoarianne5@gmail.com>
2023-01-12 09:57:52 +0800

    Working modal for deworming,php in reports

--- dist/includes/reports/deworming.php
+++ dist/includes/reports/deworming.php
@@ -1,4 +1,49 @@
 <!-- Deworming - report -->
+<div id='deworming-modal<?= $patient['deworming_id']; ?>' class="modal deworming-report__table">
+    <ul class="deworming-report__table__row deworming-report__header" role="list">
+        <li class="deworming-report__table__item">
+            Name
+        </li>
+        <li class="deworming-report__table__item">
+            Sex
+        </li>
+        <li class="deworming-report__table__item">
+            Age
+        </li>
+        <li class="deworming-report__table__item">
+            Birthdate
+        </li>
+        <li class="deworming-report__table__item">
+            Address
+        </li>
+        <li class="deworming-report__table__item">
+            Date Availed
+        </li>
+    </ul>
+
+    <ul class="deworming-report__table__row deworming-report__item" role="list">
+        <li class="deworming-report__table__item">
+            <?= $patient['firstname'] . " " . $patient['lastname']; ?>
+        </li>
+        <li class="deworming-report__table__item">
+            <?= $patient['sex']; ?>
+        </li>
+        <li class="deworming-report__table__item">
+            <?= $patient['age']; ?>
+        </li>
+        <li class="deworming-report__table__item">
+            <?= $patient['birthdate']; ?>
+        </li>
+        <li class="deworming-report__table__item">
+            <?= $patient['street_address'] . " " . $patient['barangay'] . " " . $patient['city']; ?>
+        </li>
+        <li class="deworming-report__table__item">
+            <?= $patient['deworming_date']; ?>
+        </li>
+    </ul>
+
+</div>
+
 <div id='deworming-reports' class="modal deworming-reports">
     <h4 class="search-and-destroy__report__title">
         Daily Activity (Deworming)
@@ -44,52 +89,6 @@
     <button type="submit" class="btn-green btn-add services__btn btn-print" onclick="window.print();">
         Print
     </button>
-</div>
-
-
-<div id='deworming-modal<?= $patient['deworming_id']; ?>' class="modal deworming-report__table">
-    <ul class="deworming-report__table__row deworming-report__header" role="list">
-        <li class="deworming-report__table__item">
-            Name
-        </li>
-        <li class="deworming-report__table__item">
-            Sex
-        </li>
-        <li class="deworming-report__table__item">
-            Age
-        </li>
-        <li class="deworming-report__table__item">
-            Birthdate
-        </li>
-        <li class="deworming-report__table__item">
-            Address
-        </li>
-        <li class="deworming-report__table__item">
-            Date Availed
-        </li>
-    </ul>
-
-    <ul class="deworming-report__table__row deworming-report__item" role="list">
-        <li class="deworming-report__table__item">
-            <?= $patient['firstname'] . " " . $patient['lastname']; ?>
-        </li>
-        <li class="deworming-report__table__item">
-            <?= $patient['sex']; ?>
-        </li>
-        <li class="deworming-report__table__item">
-            <?= $patient['age']; ?>
-        </li>
-        <li class="deworming-report__table__item">
-            <?= $patient['birthdate']; ?>
-        </li>
-        <li class="deworming-report__table__item">
-            <?= $patient['street_address'] . " " . $patient['barangay'] . " " . $patient['city']; ?>
-        </li>
-        <li class="deworming-report__table__item">
-            <?= $patient['deworming_date']; ?>
-        </li>
-    </ul>
-
 </div>
 
 
