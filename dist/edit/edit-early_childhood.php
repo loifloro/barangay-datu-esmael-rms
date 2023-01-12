6a53a1850ad52ebcae04d2c25956245243be8dce
loifloro <jlois.floro@gmail.com>
2023-01-12 20:40:50 +0800

    Added icon for the title tab

--- dist/edit/edit-early_childhood.php
+++ dist/edit/edit-early_childhood.php
@@ -17,6 +17,7 @@ hide_content_forms();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./assets/img/icon.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/main.css">
     <script src="/barangay-datu-esmael-rms/node_modules/sweetalert2/dist/sweetalert2.all.js"></script>
     <title>Edit Early-childhood</title>
