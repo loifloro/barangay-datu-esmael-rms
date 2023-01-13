222366d115d3c24fc7c69e30b729ec09011970f3
loifloro <jlois.floro@gmail.com>
2023-01-13 13:29:41 +0800

    Fixed path of favicon

--- dist/add/add-child_care-male.php
+++ dist/add/add-child_care-male.php
@@ -14,7 +14,7 @@ if ((!isset($_SESSION['account_id']) || !isset($_SESSION['phone_num'])) || !isse
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="./assets/img/icon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/img/icon.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/main.css">
     <script src="/barangay-datu-esmael-rms/node_modules/sweetalert2/dist/sweetalert2.all.js"></script>
     <title>Add Target Client list for Child Care Male</title>
