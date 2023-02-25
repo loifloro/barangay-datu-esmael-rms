c495476fa8d614229d382f9276825d562bbb024f
arianne05 <quimpoarianne5@gmail.com>
2023-02-25 09:06:45 +0800

    Updated pagination

--- dist/services.php
+++ dist/services.php
@@ -488,7 +488,7 @@ hide_content();
                 $page = 1;
             }
             $num_per_page = 9;
-            $start_from = ($page - 1) * 02;
+            $start_from = ($page - 1) * $num_per_page;
             // END OF PAGINATION COUNTER
 
             $query = "SELECT * FROM deworming WHERE archive_label='' LIMIT $start_from, $num_per_page";
@@ -681,7 +681,7 @@ hide_content();
                     $page = 1;
                 }
                 $num_per_page = 9;
-                $start_from = ($page - 1) * 02;
+                $start_from = ($page - 1) * $num_per_page;
                 // END OF PAGINATION COUNTER
 
                 $query = "SELECT * FROM consultation WHERE archive_label='' LIMIT $start_from, $num_per_page";
@@ -856,7 +856,7 @@ hide_content();
                 $page = 1;
             }
             $num_per_page = 9;
-            $start_from = ($page - 1) * 02;
+            $start_from = ($page - 1) * $num_per_page;
             // END OF PAGINATION COUNTER
 
             $query = "SELECT * FROM prenatal WHERE archive_label='' LIMIT $start_from, $num_per_page";
@@ -1028,7 +1028,7 @@ hide_content();
                 $page = 1;
             }
             $num_per_page = 9;
-            $start_from = ($page - 1) * 02;
+            $start_from = ($page - 1) * $num_per_page;
             // END OF PAGINATION COUNTER
 
             $query = "SELECT * FROM postnatal WHERE archive_label='' LIMIT $start_from, $num_per_page";
@@ -1202,7 +1202,7 @@ hide_content();
                 $page = 1;
             }
             $num_per_page = 9;
-            $start_from = ($page - 1) * 02;
+            $start_from = ($page - 1) * $num_per_page;
             // END OF PAGINATION COUNTER
 
             $query = "SELECT * FROM search_destroy WHERE archive_label='' LIMIT $start_from, $num_per_page";
@@ -1379,7 +1379,7 @@ hide_content();
                 $page = 1;
             }
             $num_per_page = 9;
-            $start_from = ($page - 1) * 02;
+            $start_from = ($page - 1) * $num_per_page;
             // END OF PAGINATION COUNTER
 
             $query = "SELECT * FROM early_childhood WHERE archive_label='' LIMIT $start_from, $num_per_page";
