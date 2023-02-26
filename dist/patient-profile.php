727c45e8a7c3933c9b7e7cbd0710defaf1ac39de
loifloro <jlois.floro@gmail.com>
2023-02-26 20:01:54 +0800

    Fixed wrong closing tags

--- dist/patient-profile.php
+++ dist/patient-profile.php
@@ -272,14 +272,14 @@ if (mysqli_num_rows($query_run2) > 0) {
                     <p class="sidebar__caption">Services</p>
                 </a>
             </li>
-            <a href="dashboard-masterlist.php" class="sidebar__link">
-                <li class="sidebar__item" id="masterlist_sidebar"><!--added ID-->
+            <li class="sidebar__item" id="masterlist_sidebar"><!--added ID-->
+                <a href="dashboard-masterlist.php" class="sidebar__link">
                     <svg alt="Masterlist" role="listitem" class="sidebar__icon" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="M13,14H9a1,1,0,0,0,0,2h4a1,1,0,0,0,0-2ZM17,4H15.82A3,3,0,0,0,13,2H11A3,3,0,0,0,8.18,4H7A3,3,0,0,0,4,7V19a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V7A3,3,0,0,0,17,4ZM10,5a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1V6H10Zm8,14a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V7A1,1,0,0,1,7,6H8V7A1,1,0,0,0,9,8h6a1,1,0,0,0,1-1V6h1a1,1,0,0,1,1,1Zm-3-9H9a1,1,0,0,0,0,2h6a1,1,0,0,0,0-2Z" />
                     </svg>
                     <p class="sidebar__caption">Masterlist</p>
-                </li>
-            </a>
+                </a>
+            </li>
             <li class="sidebar__item sidebar__item--margin-top" id="setting_sidebar"> <!--href link added-->
                 <a href="user-profile.php" class="sidebar__link">
                     <svg alt='Profile' role="listitem" class="sidebar__icon" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 32 32" viewBox="0 0 32 32">
