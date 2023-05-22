80c3140b78b611894218c73350af3b794803b0e8
loifloro <jlois.floro@gmail.com>
2023-05-22 23:34:05 +0800

    Uncomment logout function

--- patient-profile.php
+++ patient-profile.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 include 'includes/connection.php';
-// if ((!isset($_SESSION['account_id']) || !isset($_SESSION['phone_num']))) {
-//     header("Location: index.php?error=You are not logged in"); /*Redirect to this page if successful*/
-//     exit();
-// }
-//FUNCTION TO HIDE CONTENT BASED ON USER LEVEL
+if ((!isset($_SESSION['account_id']) || !isset($_SESSION['phone_num']))) {
+    header("Location: index.php?error=You are not logged in"); /*Redirect to this page if successful*/
+    exit();
+}
+// FUNCTION TO HIDE CONTENT BASED ON USER LEVEL
 $email = $_SESSION['user_email'];
 
 // PATIENT ACCESS
